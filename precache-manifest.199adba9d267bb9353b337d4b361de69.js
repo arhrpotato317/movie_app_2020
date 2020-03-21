d192b83c90ed75015cb8127426263c281e4d4052
arhrpotato317 <arhrpotato317@gmail.com>
2020-03-21 22:31:56 +0900

    Updates

---
+++ precache-manifest.199adba9d267bb9353b337d4b361de69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb45478f14b19a593c46900927e693c1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ce1da90746de0b81ce03",
+    "url": "/movie_app_2020/static/css/main.61fb089f.chunk.css"
+  },
+  {
+    "revision": "0e16d3f86d786e10ecef",
+    "url": "/movie_app_2020/static/js/2.844389f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.844389f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1da90746de0b81ce03",
+    "url": "/movie_app_2020/static/js/main.fbda3fb9.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
